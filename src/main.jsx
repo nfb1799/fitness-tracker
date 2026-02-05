bd332a6501ceb5c270177650cf08a4ce635a420c
nfb1799 <nfb1799@rit.edu>
2026-02-05 10:03:42 -0500

    Remove force update logic entirely

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext'
-import { UpdateProvider } from './components/UpdatePrompt'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UpdateProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </UpdateProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
